d646259db60f7e71d44d1b2fd1df442410c921ed
corly meng <corlymeng@qq.com>
2016-09-11 00:38:48 +0800

    Merge branch 'master' of github.com:CorlyDream/algorithm
    # Conflicts:
    #	src/main/java/test/Main6.java

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,5 +3,8 @@ package test;
 /**
  * Created by corly on 2016/8/7.
  */
-public interface Test {
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(4>>1);
+    }
 }
